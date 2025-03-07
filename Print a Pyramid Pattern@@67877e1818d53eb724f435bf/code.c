ad725b23fa2a8ba3788a4820bcb1eb3cbdd4b370
arzoo56 <arzoochugh07@gmail.com>
2025-03-07 16:55:51 +0530

    Solution to Print a Pyramid Pattern

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -3,14 +3,13 @@ int main(){
     int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            if(i%2==0 || j%2==0){
-                printf("*");
-            }
-            else{
+        for(int j=1;j<=n-i;j++){
             printf(" ");
-        }}
-    printf("\n");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
     }
-return 0;
+    return 0;
 }
